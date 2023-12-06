7939292d7663b5129d6a074fefe6b37fca4eea94
shikoqu <shikoqu@gmail.com>
2023-12-06 21:11:09 +0100

    crossover and mutation added

--- OurGP/src/Node.java
+++ OurGP/src/Node.java
@@ -2,42 +2,107 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 enum Rules {
-    PROGRAM,
-    EXPR,
-    IF,
-    WHILE,
-    BLOCK,
-    PRINT,
-    SCAN,
-    ASSIGN,
-    COMPARISON,
-    LOGIC,
-    ARITHMETIC,
-    TRIG,
-    BOOL_VALUE,
-    NUMERIC_VALUE
+    PROGRAM(null),
+    EXPR(null),
+    IF(EXPR),
+    WHILE(EXPR),
+    BLOCK(EXPR),
+    PRINT(EXPR),
+    SCAN(EXPR),
+    ASSIGN(EXPR),
+    BOOL_VALUE(null),
+    NUMERIC_VALUE(null),
+    COMPARISON(BOOL_VALUE),
+    LOGIC(BOOL_VALUE),
+    ARITHMETIC(NUMERIC_VALUE),
+    TRIG(NUMERIC_VALUE);
+
+    private final Rules dependency;
+
+    Rules(Rules dependency) {
+        this.dependency = dependency;
+    }
+
+    public Rules getDependency() {
+        if (this.dependency == null)
+            return this;
+        return dependency;
+    }
 }
 
 abstract class Node {
     Node parent;
     int depth;
-    int max_depth = 30;
+    Rules rule;
+    static final int max_depth = 30;
+    static final double mutation_probability = 0.05;
+
+    Node copy() { return copy(null); }
+    abstract Node copy(Node parent);
+    abstract int length();
+    abstract Node getNode(int index);
+    ArrayList<Node> getNodes(Rules rule) {
+        return getNodes(rule, new ArrayList<Node>());
+    }
+    abstract ArrayList<Node> getNodes(Rules rule, ArrayList<Node> nodes);
+
+    Rules getRuleClass() {
+        return this.rule.getDependency();
+    }
+
+    abstract void mutate();
 }
 
 class LeafNode extends Node {
     String token;
 
     LeafNode(Node parr, String tok) {
+        this.rule = null;
         this.parent = parr;
         this.token = tok;
         this.depth = parent.depth+1;
+    }
+
+    LeafNode copy(Node parent) {
+        return new LeafNode(parent, this.token);
+    }
+
+    int length() { return 1; }
+    Node getNode(int index) { return null; }
+    ArrayList<Node> getNodes(Rules rule, ArrayList<Node> nodes) {
+        return nodes;
+    }
+
+    void mutate() {
+        if (Math.random() > mutation_probability) return;
+
+        Random random = new Random();
+        String[] compSymbols = {"==", "!=", "<", "<=", ">", ">="};
+        String[] logicSymbols = {"&&", "||"};
+        String[] arithSymbols = {"+", "-", "*", "/", "%"};
+        String[] trigSymbols = {"sin", "cos"};
+
+        switch (token) {
+            case "==", "!=", "<", "<=", ">", ">=":
+                this.token = compSymbols[random.nextInt(6)];
+                break;
+            case "&&", "||":
+                this.token = logicSymbols[random.nextInt(2)];
+                break;
+            case "+", "-", "*", "/", "%":
+                this.token = arithSymbols[random.nextInt(5)];
+                break;
+            case "sin", "cos":
+                this.token = trigSymbols[random.nextInt(2)];
+                break;
+        }
     }
 }
 
 class RuleNode extends Node {
     List<Node> children;
-    Rules rule;
 
     RuleNode(Node parr, Rules r) {
         this.parent = parr;
@@ -75,9 +140,9 @@ class RuleNode extends Node {
         return "X" + String.valueOf(random.nextInt(100));
     }
 
-    void grow() {
+    void grow(int max_depth) {
         Random random = new Random();
-        String[] compSymbols = {"=", "!=", "<", "<=", ">", ">="};
+        String[] compSymbols = {"==", "!=", "<", "<=", ">", ">="};
         String[] logicSymbols = {"&&", "||"};
         String[] arithSymbols = {"+", "-", "*", "/", "%"};
         String[] trigSymbols = {"sin", "cos"};
@@ -157,4 +222,51 @@ class RuleNode extends Node {
                 break;
         }
     }
+
+    RuleNode copy(Node parent) {
+        RuleNode copy = new RuleNode(parent, this.rule);
+        for (Node child : this.children)
+            copy.children.add(child.copy(copy));
+
+        return copy;
+    }
+
+    int length() {
+        int len = 1;
+        for (Node child : this.children)
+            len += child.length();
+
+        return len;
+    }
+
+    Node getNode(int index) {
+        if (index == 0)
+            return this;
+        else {
+            for (Node child : this.children) {
+                if (index < child.length())
+                    return child.getNode(index-1);
+                else
+                    index -= child.length();
+            }
+            return null;
+        }
+    }
+
+    ArrayList<Node> getNodes(Rules rule, ArrayList<Node> nodes) {
+        if (this.rule.getDependency() == rule)
+            nodes.add(this);
+
+        for (Node child : this.children)
+            child.getNodes(rule, nodes);
+
+        return nodes;
+    }
+
+    void mutate() {
+        for (Node child : this.children)
+            child.mutate();
+    }
+
+
 }
--- OurGP/src/Program.java
+++ OurGP/src/Program.java
@@ -1,3 +1,59 @@
+import java.util.List;
+import java.util.Random;
+
 public class Program {
-    
+    RuleNode root;
+    Random rd = new Random();
+
+    //! ---------- CONSTRUCTORS ----------
+    /**
+     * <h1>Generates a random program.</h1>
+     * Max depth of this program is defined by int parameter in grow method.
+     * Default max depth is 5.
+     */
+    public Program() {
+        root = new RuleNode(null, Rules.PROGRAM);
+        root.grow(5);
+    }
+    /**
+     * <h1>Generates a random program.</h1>
+     * Max depth of this program is defined by int parameter in grow method.
+     * @param max_depth max depth of this program
+     */
+    public Program(int max_depth) {
+        root = new RuleNode(null, Rules.PROGRAM);
+        root.grow(max_depth);
+    }
+
+    public Program(Program program) {
+        root = (RuleNode) program.root.copy();
+    }
+
+
+    //! ---------- GENETIC OPERATORS ----------
+    public Program crossover(Program parent1, Program parent2) {
+        Program offspring = new Program(parent1);
+        
+        Node crossoverNode1 = offspring.root.getNode(rd.nextInt(offspring.root.length()));
+        if (crossoverNode1 == null)
+            return offspring;
+        
+        List<Node> potentialCrossoverNodes = parent2.root.getNodes(crossoverNode1.rule.getDependency());
+
+        if (potentialCrossoverNodes.size() == 0)
+            return offspring;
+        
+        Node crossoverNode2 = potentialCrossoverNodes.get(rd.nextInt(potentialCrossoverNodes.size()));
+
+        crossoverNode1 = crossoverNode2.copy(crossoverNode1.parent);
+
+        return offspring;
+    }
+
+    public Program mutation(Program parent) {
+        Program offspring = new Program(parent);
+        offspring.root.mutate();
+
+        return offspring;
+    }
 }
